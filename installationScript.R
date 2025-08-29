f7d50002c04c0d73cdbe82fac4a836324b3c0436
lshep <lori.shepherd@roswellpark.org>
2025-08-29 11:32:46 -0400

    Add Rproj / Rstudio section Add a comparison of base R data.frame and plotting to  tidy and ggplot2

--- installationScript.R
+++ installationScript.R
@@ -1,6 +1,6 @@
 install.packages("BiocManager")
 
 pkgList = c("ggplot2", "dplyr", "knitr", "readr", "readxl", "writexl",
-            "RColorBrewer", "UpSetR", "swirl", "rmarkdown")
+            "RColorBrewer", "UpSetR", "swirl", "rmarkdown", "tidyr")
 
 BiocManager::install(pkgList)
